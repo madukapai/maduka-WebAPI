b2c682c5cba1eec0cca0b0a9c8af63eaee5bd74d
maduka <maduka_bor@hotmail.com>
2019-01-06 20:27:21 +0800

    增加ValidationContext的功能

---
+++ maduka-WebAPI/Controllers/ValidController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace maduka_WebAPI.Controllers
+{
+    public class ValidController : ApiController
+    {
+        /// <summary>
+        /// 執行資料寫入的Post動作
+        /// </summary>
+        /// <param name="query"></param>
+        public Models.ValidModels.ValidInfoResult Post(Models.ValidModels.ValidInfoQuery query)
+        {
+            Models.ValidModels.ValidInfoResult result = new Models.ValidModels.ValidInfoResult() { IsValid = true, };
+            result.List = new List<Models.ValidModels.ValidInfoResult.ValidItem>();
+
+            #region // 傳統欄位驗證的寫法
+            //if (string.IsNullOrEmpty(query.Name))
+            //{
+            //    result.IsValid = false;
+            //    result.List.Add(new Models.ValidModels.ValidInfoResult.ValidItem()
+            //    {
+            //        Field = new List<string> { "Name" },
+            //        Message = "Name欄位必填"
+            //    });
+            //}
+
+            //if (query.Age > 130 || query.Age < 1)
+            //{
+            //    result.IsValid = false;
+            //    result.List.Add(new Models.ValidModels.ValidInfoResult.ValidItem()
+            //    {
+            //        Field = new List<string> { "Age" },
+            //        Message = "Age欄位必須在1與130之間"
+            //    });
+            //}
+            #endregion
+
+
+            #region // 使用ValidationContext的驗證物件
+            // 定義ValidationContext的驗證物件
+            var context = new ValidationContext(query);
+
+            // 定義進行Validation回傳的訊息
+            var validationResults = new List<ValidationResult>();
+
+            // 進行驗證動作
+            bool isValid = Validator.TryValidateObject(query, context, validationResults, true);
+
+            // 將驗證結果進行處理，並回傳到指定的回傳物件中
+            result.IsValid = isValid;
+            result.List = validationResults.Select(c => new Models.ValidModels.ValidInfoResult.ValidItem()
+                                            {
+                                                Field = c.MemberNames,
+                                                Message = c.ErrorMessage
+                                            })
+                                            .ToList();
+            #endregion
+
+            return result;
+        }
+    }
+}
---
+++ maduka-WebAPI/Models/ValidModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace maduka_WebAPI.Models
+{
+    public class ValidModels
+    {
+        /// <summary>
+        /// 輸入資料的模型
+        /// </summary>
+        public class ValidInfoQuery
+        {
+            [Required]
+            public string Name { get; set; }
+            public string Tel { get; set; }
+            public string Address { get; set; }
+            [Required]
+            [Range(1, 130)]
+            public int Age { get; set; }
+            [Required]
+            public DateTime Birthday { get; set; }
+        }
+
+        /// <summary>
+        /// 回傳驗證結果的模型
+        /// </summary>
+        public class ValidInfoResult
+        {
+            public bool IsValid { get; set; }
+            public List<ValidItem> List { get; set; }
+            public class ValidItem
+            {
+                public IEnumerable<string> Field { get; set; }
+                public string Message { get; set; }
+            }
+        }
+    }
+}
